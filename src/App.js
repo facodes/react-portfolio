492028ea561ab109d7311da9508cd41d9d1fabc7
felix <felixcodesg@gmail.com>
2019-09-25 21:54:22 -0400

    setting up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React , { useState, useEffect }from 'react';
+import { Route, Link } from 'react-router-dom';
+
+import contentful from "./api/contentful";
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { 
+  faBars
+} from '@fortawesome/free-solid-svg-icons';
+
+import { fab, faGithub} from '@fortawesome/free-brands-svg-icons';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+
+// Pages 
+import Home from './pages/Home';
+import Project from './pages/Project';
+
+library.add(
+  fab,
+  faBars,
+  faGithub,
+ );
 
 function App() {
+
+	const [projects, setProjects] = useState([]);
+
+	console.log (contentful);
+
+	useEffect(() => {
+    async function getProjects() {
+      const { items } = await contentful.getEntries({
+        content_type: "project"
+      });
+    }
+
+    getProjects();
+  }, [projects]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+			<Link to="/">Home</Link>
+			<Link to="/project">Poject</Link>
+
+    	<Route exact path="/" component={Home}/>
+    	<Route path="/project" component={Project}/>
     </div>
   );
 }
