adefffc28ea6389363d0ab359269acd59c47eedf
felix <felixcodesg@gmail.com>
2019-09-27 06:20:19 -0400

    added styled to heading component

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -2,7 +2,25 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledHeading = styled.h1`
-	color: ${props => props.color ? `var(--color-${props.color})` : `inherit`};
+	display: inline-block;
+	color: ${props => props.color ? `var(--color-${props.color})` : `var(--color-darker)`};
+	font-size: 4rem;
+	font-weight: var(--bold);
+	text-transform: capitalize;
+	position: relative;
+	line-height: 1.2;
+
+	&::after{
+		content: '';
+		position: absolute;
+		bottom: 0;
+		right: -5px;
+		width: 70px;
+		height: 4px; 
+		background: currentColor;
+	}
+	
+
 `
 
 const Heading = (props) => {
