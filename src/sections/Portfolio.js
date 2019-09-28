a1ad34a76670759e80c330e481f21f4566fe0515
felix <felixcodesg@gmail.com>
2019-09-28 18:45:43 -0400

    added a heading, fix padding size

--- src/sections/Portfolio.js
+++ src/sections/Portfolio.js
@@ -1,33 +1,45 @@
-import React from 'react';
+import React ,{ useContext }from 'react';
 import styled from 'styled-components';
+
+import { ProjectsContext } from '../Store';
 
 // Components
 import Heading from '../components/Heading';
 import ProjectItem from '../components/ProjectItem';
 
-
 import API from '../API';
 
+
 const  Wrapper = styled.section`
-	padding: 1.6em 5em;
+	padding: 6em 1.5em;
 	background: var(--color-darker);
 	color: var(--color-white);
 	z-index: 2;
 	position: relative;
+`
 
+const ProjectsWrapper = styled.div`
+	margin-top: 6rem;
 `
 
 const ProjectList = (props) => {
+	const [projects] = useContext(ProjectsContext); 
 
-
+	console.log (projects);
 
   return (
   	<Wrapper id="portfolio">
+			<Heading outline color="light">
+				Portfolio
+			</Heading>
+	
+		 <ProjectsWrapper>
 			{
-				API.map(project => (
+				projects.map(project => (
 					<ProjectItem project={project} key={project.id} ></ProjectItem>
 				))
 			}
+		 </ProjectsWrapper>
   	</Wrapper>
   )
 }
